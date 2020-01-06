bfbb858b16a112d2f3b306bda3da7c11faaff7b4
Harsha Annamareddy <Harsha.a@Technovert.in>
2020-01-06 19:38:34 +0530

    created LambdaExpression File

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaExpression
+{
+    class Program
+    {
+        delegate int findcube(int x);
+        //findcube cube=x => x * x * x;
+        static void Main(string[] args)
+        {
+            Func<int,int> cube = z => z * z * z;//findcube cube=z=>z*z*z;
+            int x = cube(5);
+            Console.WriteLine(x);
+            List<int> ListObject = new List<int> { 10,20,30,40,50,60,70,80,90,100};
+            //ListObject.Select(y=>y==60)
+            Console.WriteLine(ListObject.Select(y => y == 60));
+            Console.ReadKey();
+        }
+    }
+}
